39fc63f679635311b13ff10e3f03558bfe59efe3
sijun-yang <yangsijun@bondit.io>
2024-09-23 19:01:04 +0900

    Update build.gradle.kts
    Add description About jsr305 & kotlin 'plugin.jpa' plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,18 @@ plugins {
     kotlin("plugin.spring") version "1.9.25"
     id("org.springframework.boot") version "3.3.4"
     id("io.spring.dependency-management") version "1.1.6"
+    // Kotlin No-arg compiler plugin / JPA support: https://kotlinlang.org/docs/no-arg-plugin.html#jpa-support
+    // Kotlin + JPA 사용 시 주의와 개인적인 생각이 담긴 블로그: https://colabear754.tistory.com/145
+    //   위 블로그에 추가 설명
+    //   1. (언제부턴지 모르겠으나) plugin.jpa는 spring에서 별도 설정 없이 사용 가능하도록 플러그인을 지원한다.
+    //      1-1. no-arg + all-open(@Entity, @Embeddable, @MappedSuperclass)
+    //   2. JPA Entity로 data class가 적절한가?
+    //        관련 추가 자료 - Should I use Kotlin data class as JPA entity?: https://stackoverflow.com/questions/58127353/should-i-use-kotlin-data-class-as-jpa-entity
+    //                      https://github.com/spring-guides/tut-spring-boot-kotlin?tab=readme-ov-file#persistence-with-jpa
+    //        Spring 공식문서에서는 추천하지 않는다. (처음 커밋에는 data class 예시를 사용하다가 v2로 문서를 업데이트하면서 더 이상 data class를 사용하지 않는다. 참고: https://github.com/spring-guides/tut-spring-boot-kotlin/commit/7041397421f20d251cb2ee0b5e9c5ef809804410)
+    //        상속이 불가능하므로 확장성을 고려해야 하고, 이후에 변경해야 하는 불편함을 겪을 수 있음.
+    //        그러나 순환참조는 어디서나 발생할 수 있고, equals()와 hashCode() 문제는 BaseEntity를 상속하는 것으로 해결 가능하다.
+    //        개인적으로는 data class가 편리한 부분이 많았고, 필요에 따라 충분히 선택할 수 있다고 생각한다. (물론 적극적인 추천은 하지 않지만...)
     kotlin("plugin.jpa") version "1.9.25"
 }
 
@@ -34,6 +46,10 @@ dependencies {
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
+// Refer to the following links for more information:
+// Kotlin Support: https://docs.spring.io/spring-boot/reference/features/kotlin.html#features.kotlin.null-safety
+// Calling Java from Kotlin: https://kotlinlang.org/docs/java-interop.html#jsr-305-support
+// JSR-305: https://jcp.org/en/jsr/detail?id=305
 kotlin {
     compilerOptions {
         freeCompilerArgs.addAll("-Xjsr305=strict")
